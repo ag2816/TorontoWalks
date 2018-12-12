1210f30f7f1b0bb2e9bc9cb8d5ecb462faf4e4d3
ag2816 <ae.gordon@yahoo.ca>
2018-12-12 13:56:40 -0500

    clean up similarity calcs

--- web/find_stops.py
+++ web/find_stops.py
@@ -8,29 +8,71 @@ from hdbscan import HDBSCAN
 from sklearn.metrics.pairwise import cosine_similarity
 from scipy import spatial
 
-def find_similarity(df_features, df_user,  df_poi, sim_method=cosine_similarity):
+def find_similarity(df_features, df_user,  df_poi, sim_method='cosine_similarity'):
     '''
     INPUTS: "vectorized" dataframes of POIS and User
+    sim_method:
+        * cosine_similarity=
+        * spatial =
+        * hamming
     RETURNS: dataframe of Points of Interest sorted from highest to lowest similarity rating to the user prefs
     '''
     df_poi['sim_rating'] = 0
-    if sim_method ==cosine_similarity:
+
+    types_weight_percent = 0.6
+    prefs_weight_percent = 0.2
+    ages_weight_percent = 0.2
+
+    if sim_method =='cosine_similarity':
+        # print('cosine_similarity')
+        # avail_interests = list(df_features.columns)
+        # types_list = [i for i, x in enumerate(avail_interests) if 'poi_type_' in x]
+        # types_sim=cosine_similarity(df_features.iloc[:,types_list], df_user.iloc[:,types_list])
+        #
+        # prefs_list = [i for i, x in enumerate(avail_interests) if 'pref_' in x]
+        # prefs_sim = cosine_similarity(df_features.iloc[:,prefs_list], df_user.iloc[:,prefs_list])
+        #
+        # ages_list = [i for i, x in enumerate(avail_interests) if 'build_century_' in x]
+        # ages_sim = cosine_similarity(df_features.iloc[:,ages_list], df_user.iloc[:,ages_list])
+        #
+        # #print(types_sim, prefs_sim, ages_sim)
+        #
+        # cosine_sim=(types_sim * types_weight_percent) + (prefs_sim *prefs_weight_percent) + (ages_sim *ages_weight_percent)
+
         cosine_sim = cosine_similarity(df_features,df_user)
         user_matches = pd.DataFrame(cosine_sim, columns=['user_match']) # convert to df for ease
-        user_matches.sort_values('user_match', ascending=False, inplace=True) # sort from best to worst matches
-       # for ix,row in user_matches.iloc[0:20,:].iterrows():
-        for ix,row in user_matches.iterrows():
-            # now find matches close to target of interest - ix is row in dataframe that matches to user match
-            df_poi.loc[ix,'sim_rating'] = row.user_match
+        df_poi['sim_rating'] = user_matches
+
+       # # cosine_sim = cosine_similarity(df_features,df_user)
+       #  user_matches = pd.DataFrame(cosine_sim, columns=['user_match']) # convert to df for ease
+       #  user_matches.sort_values('user_match', ascending=False, inplace=True) # sort from best to worst matches
+       #
+       #  for ix,row in user_matches.iterrows():
+       #      # now find matches close to target of interest - ix is row in dataframe that matches to user match
+       #      df_poi.loc[ix,'sim_rating'] = row.user_match
     else:
+
+        # update weights to put priority on choice of building / plaque / art
+        num_features = len(list(df_features.columns))
+        weight_of_importance=np.ones(num_features)
+        weight_of_importance[-3:]= 10#.2
+
+#         weight_of_importance[0:-3]=.4/(num_features-3)
+#         weight_of_importance
+
         # spatial method requires numpy arrays
-        np_features = df_features.as_matrix()
-        U=df_user.as_matrix()
-       # weight_of_importance=[0.05,0.05,0.05,0.05,.27,.26,.27]
+        np_features = df_features.values #as_matrix()
+        U=df_user.values #as_matrix()
+
         site_prefs=[]
         for i in range(0,len(np_features)):
             np_features[i,:]
-            result = spatial.distance.cosine(np_features[i,:], U)#, weight_of_importance)
+            if sim_method== 'spatial':
+               # print('spatial')
+                result = spatial.distance.cosine(np_features[i,:], U, weight_of_importance)
+            else:
+                #print('hamming')
+                result = spatial.distance.hamming(np_features[i,:], U)
             res_dict = {'ix': i, 'dist': result}
             site_prefs.append(res_dict)
         # convert similarity rating for each POI to a df
@@ -41,8 +83,47 @@ def find_similarity(df_features, df_user,  df_poi, sim_method=cosine_similarity)
             # now find matches close to target of interest
             df_poi.loc[ix,'sim_rating'] = row.dist
 
+
     df_poi.sort_values('sim_rating', inplace=True, ascending=False)
     return df_poi
+
+
+#
+# def find_similarity(df_features, df_user,  df_poi, sim_method=cosine_similarity):
+#     '''
+#     INPUTS: "vectorized" dataframes of POIS and User
+#     RETURNS: dataframe of Points of Interest sorted from highest to lowest similarity rating to the user prefs
+#     '''
+#     df_poi['sim_rating'] = 0
+#     if sim_method ==cosine_similarity:
+#         cosine_sim = cosine_similarity(df_features,df_user)
+#         user_matches = pd.DataFrame(cosine_sim, columns=['user_match']) # convert to df for ease
+#         user_matches.sort_values('user_match', ascending=False, inplace=True) # sort from best to worst matches
+#        # for ix,row in user_matches.iloc[0:20,:].iterrows():
+#         for ix,row in user_matches.iterrows():
+#             # now find matches close to target of interest - ix is row in dataframe that matches to user match
+#             df_poi.loc[ix,'sim_rating'] = row.user_match
+#     else:
+#         # spatial method requires numpy arrays
+#         np_features = df_features.as_matrix()
+#         U=df_user.as_matrix()
+#        # weight_of_importance=[0.05,0.05,0.05,0.05,.27,.26,.27]
+#         site_prefs=[]
+#         for i in range(0,len(np_features)):
+#             np_features[i,:]
+#             result = spatial.distance.cosine(np_features[i,:], U)#, weight_of_importance)
+#             res_dict = {'ix': i, 'dist': result}
+#             site_prefs.append(res_dict)
+#         # convert similarity rating for each POI to a df
+#         df_site_prefs = pd.DataFrame(site_prefs)
+#         df_site_prefs.sort_values('dist', ascending=False, inplace=True)
+#
+#         for ix,row in df_site_prefs.iterrows():
+#             # now find matches close to target of interest
+#             df_poi.loc[ix,'sim_rating'] = row.dist
+#
+#     df_poi.sort_values('sim_rating', inplace=True, ascending=False)
+#     return df_poi
 
 def find_points_in_area(df, lat, long, num_points, max_distance, inflate_amount = 1.4):
     '''
